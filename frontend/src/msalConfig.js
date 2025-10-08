9f323ff2b3bdd75db91421a3e25fcdf12896fdcd
MaxShoener <meshoener@gmail.com>
2025-10-08 09:22:21 -0400

    Update msalConfig.js

--- frontend/src/msalConfig.js
+++ frontend/src/msalConfig.js
@@ -1,1 +1,7 @@
-
+export const msalConfig = {
+  auth: {
+    clientId: "YOUR_AZURE_APP_CLIENT_ID",
+    authority: "https://login.microsoftonline.com/common",
+    redirectUri: window.location.origin
+  }
+};
