2464b29d376a633d1ab840ccb7452164ee4d5691
MaxShoener <meshoener@gmail.com>
2025-10-09 11:23:39 -0400

    Delete frontend directory

--- frontend/src/msalConfig.js
+++
@@ -1,7 +0,0 @@
-export const msalConfig = {
-  auth: {
-    clientId: "YOUR_AZURE_APP_CLIENT_ID",
-    authority: "https://login.microsoftonline.com/common",
-    redirectUri: window.location.origin
-  }
-};
